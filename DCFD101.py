457d70e0eb8e4bd51bf9740fd26c943fb9725422
Jrossi11 <71575042+Jrossi11@users.noreply.github.com>
2021-10-03 14:21:30 +0200

    Update DCFD101.py

--- DCFD101.py
+++ DCFD101.py
@@ -4,11 +4,11 @@ Jero & Juan
 """
 import pandas as pd
 import numpy as np
-import yfinance as yf
+import time
 import matplotlib.pyplot as plt
 import random as rd
 import string
-import asyncio
+#import asyncio
 
 
 # Definir su activo subyacente
@@ -23,7 +23,7 @@ def brownian_motion(S0, u, rf, num_days, sigma):
         price_series.append(price_series[-1]*(1+u*dt + sigma * np.random.normal(0, 1) * np.sqrt(dt)))
     return price_series 
 
-subyacente = brownian_motion(146, 0.1, 0.025, 90, 0.1)
+subyacente = brownian_motion(146, 0.1, 0.025, 10, 0.1)
 
 # Definimos los agentes involucrados dado que conectaron sus billeteras virtuales
 class user:    
@@ -61,147 +61,171 @@ def create_orders(buy=True, market_price=0):
         address = [''.join(rd.choice(adress_gen) for i in range(10)) for j in range(3)]
         return [list(i) for i in zip(sell_orders,sell_orders_size, address)]
 
-trades = pd.DataFrame(columns=['Price','Size', 'Underlying Market Price'])
+trades = pd.DataFrame(columns=['Price','Size', 'Underlying Market Price', 'Contract id'])
 
 order_book_b = pd.DataFrame(create_orders(), columns=['Price', 'Size', 'Address']).sort_values(by=['Price'], ascending=False).reset_index(drop=True)
 order_book_s = pd.DataFrame(create_orders(False), columns=['Price', 'Size', 'Address']).sort_values(by=['Price']).reset_index(drop=True)
 
+sent_orders = 0
+def add_user_order(entryprice, direction, cantidad, leverage):
 
+    global order_book_b, order_book_s, init_margin_long, init_margin_short, sent_orders    
 
-#-----Entran los datos de la interfaz-----#
-leverage, cantidad, entryprice_long, entryprice_short = 10, 1, subyacente[0], subyacente[0]
-
-
-
-class user_order:
-        self.leverage = leverage
-        self.cantidad = cantidad
-        self.direction = direction
-        self.entry_price = entry_price              #User inputs... el active = true de user lo pondría aca.
-        self.state = state 
+    while sent_orders < 3:
+        time.sleep(3) 
+        # Calculo el margen inicial
+        init_margin_long =  entryprice * cantidad / leverage
+        init_margin_short = entryprice * cantidad / leverage
+        #Checkeo de si les dá el balance en USDT para entrar en el contrato
+        if init_margin_long > user_long.usdt:
+            print("Long cannot enter position, margin need's more funds !")
+        elif init_margin_short > user_short.usdt:
+            print("Short cannot enter position, margin need's more funds !")
+        else:
+            print('Trade can be entered')
         
-user_order(10, 1, "long", subyacente[0], "sent") #sent / matched / closed
+        if direction == 'long':
+        #Agrego la orden del usuario
+            order_book_b.loc[len(order_book_b)+1] = [entryprice, cantidad, user_long.wallet_id]
+            order_book_b = order_book_b.sort_values(by=['Price'], ascending=False).reset_index(drop=True)
+            
+        if direction == 'sell':
+            order_book_s.loc[len(order_book_s)+1] = [entryprice, cantidad, user_short.wallet_id]
+            order_book_s = order_book_s.sort_values(by=['Price']).reset_index(drop=True)
+            
+        sent_orders += 1 
+
+        yield
+        
+init_margin_long, init_margin_short = 0, 0    
 
 
-
-
-
-
-
-
-
-#Agrego la orden del usuario
-order_book_b.loc[len(order_book_b)+1] = [entryprice_long,cantidad, user_long.wallet_id]
-order_book_b = order_book_b.sort_values(by=['Price'], ascending=False).reset_index(drop=True)
-
-#Devolver cuanto margen necesita poner cada usuario
-init_margin_long =  entryprice_long * cantidad / leverage
-init_margin_short = entryprice_short * cantidad / leverage
-
-#Checkeo de si les dá el balance en USDT para entrar en el contrato
-if init_margin_long > user_long.usdt:
-    print("Long cannot enter position, margin need's more funds !")
-elif init_margin_short > user_short.usdt:
-    print("Short cannot enter position, margin need's more funds !")
-
-df = pd.DataFrame({'Price':subyacente[0], 'margin_long':init_margin_long, 'margin_short':init_margin_short,
-                  'pnl_long':0, 'pnl_short':0, 'state_long':np.nan, 'state_short':np.nan, 'risk_long':0, 'risk_short':0}, index=[0])
-
-trade_num = 0
-long_add = 0
-short_add = 0
-#while df.loc[i-1,'state_long'] != 'Liquidated' and df.loc[i-1,'state_short'] != 'Liquidated':
-for i in range(1,len(subyacente)):
-    
+def match_orders(i):
+    trade_num = 0
+    global order_book_b, order_book_s
     while max(order_book_b['Price']) >= min(order_book_s['Price']):
         if order_book_b['Price'][0] >= order_book_s['Price'][0]:
             trade_num += 1 
             price = order_book_b['Price'][0]
             size = min(order_book_b['Size'][0], order_book_s['Size'][0])
-            trades.loc[trade_num]=[price, size, subyacente[i]]
+            trades.loc[trade_num]=[price, size, subyacente[i], order_book_b['Address'][0]+order_book_s['Address'][0]]
             print(f'{size} matched at {round(price,2)}')
             
-            order_book_b['Size'][0] -= size 
-            order_book_s['Size'][0] -= size 
+            order_book_b.loc[0,'Size'] -= size 
+            order_book_s.loc[0,'Size'] -= size 
             
-            if order_book_b['Size'][0] == 0:
+            if order_book_b.loc[0,'Size'] == 0:
                 order_book_b = order_book_b.drop(0).reset_index(drop=True)
                 
-            if order_book_s['Size'][0] == 0:
+            if order_book_s.loc[0,'Size'] == 0:
                 order_book_s = order_book_s.drop(0).reset_index(drop=True)
+    
 
-    ###
-    if user_long.wallet_id not in order_book_b:
-        df.loc[i,'Price'] = trades['Price'][len(trades)] #Suponiendo q gabo. (en todo el script long=GABO)
-        
-        df.loc[i,"risk_long"] = leverage * (((df.loc[i,"Price"] + long_add) / entryprice_long) -1)
-        df.loc[i,"risk_short"] = -leverage * (((df.loc[i,"Price"] - short_add) / entryprice_short) -1)
-        
-        df.loc[i,"pnl_long"] = ((df.loc[i,"Price"] / entryprice_long) -1) * cantidad * entryprice_long
-        df.loc[i,"pnl_short"] = -((df.loc[i,"Price"] / entryprice_short) -1) * cantidad * entryprice_long
-        
-        df.loc[i,'margin_long'] = df.loc[i, 'pnl_long']+df.loc[0,'margin_long'] + long_add
-        df.loc[i,'margin_short'] = df.loc[i, 'pnl_short']+df.loc[0,'margin_short'] + short_add
+def main():
+    print('Main is running')
+    long_add = 0
+    short_add = 0
     
-        
-        #Long
-        if df.loc[i,"risk_long"] < -0.50 and df.loc[i,"risk_long"] > -0.8:
-            df.loc[i,"state_long"] = "Called"
-            print(df)
-            print(f"Hey, {user_long.name}, you just got a margin call ! Add more funds !")
-            
-            #Long agrega fondos
-            long_ask = float(input("Add USDT to margin:"))
-            
-            if long_ask > user_long.usdt - init_margin_long:
-                print("You don't have enough USDT in the wallet !")
-                long_add += 0
-            else:
-                long_add += long_ask
+    global order_book_b, order_book_s
     
+    for i in range(1,len(subyacente)):
+        print('Matching orders')
+        match_orders(i)
+    
+        ### SI la orden del usuario se matcheo... Mostrar el proceso
+        if user_long.wallet_id in trades['Contract id']:
             
-        elif df.loc[i,"risk_long"] < -0.8:                 
-            df.loc[i,"state_long"] = "Liquidated"
-            print("Your position has been liquidated !")
+            df = pd.DataFrame({'Price':subyacente[0], 'margin_long':init_margin_long, 
+                   'margin_short':init_margin_short,'pnl_long':0, 'pnl_short':0,
+                   'state_long':np.nan, 'state_short':np.nan, 'risk_long':0, 
+                   'risk_short':0}, index=[0])
             
+            df.loc[i,'Price'] = trades['Price'][len(trades)] #Suponiendo q gabo. (en todo el script long=GABO)
             
-        elif df.loc[i,"risk_long"] > -0.5:
-             long_add += 0
+            df.loc[i,"risk_long"] = leverage * (((df.loc[i,"Price"] + long_add) / entryprice_long) -1)
+            df.loc[i,"risk_short"] = -leverage * (((df.loc[i,"Price"] - short_add) / entryprice_short) -1)
             
-        #Short
-        if df.loc[i,"risk_short"] < -0.50 and df.loc[i,"risk_short"] > -0.8:
-            df.loc[i,"state_short"] = "Called"
-            print(f"Hey, {user_short.name}, you just got a margin call ! Add more funds !")
+            df.loc[i,"pnl_long"] = ((df.loc[i,"Price"] / entryprice_long) -1) * cantidad * entryprice_long
+            df.loc[i,"pnl_short"] = -((df.loc[i,"Price"] / entryprice_short) -1) * cantidad * entryprice_long
             
-            #Short agrega fondos
-            short_ask = float(input("Add USDT to margin:"))    
+            df.loc[i,'margin_long'] = df.loc[i, 'pnl_long']+df.loc[0,'margin_long'] + long_add
+            df.loc[i,'margin_short'] = df.loc[i, 'pnl_short']+df.loc[0,'margin_short'] + short_add
+        
             
-            if short_ask > user_short.usdt - init_margin_short:
-                print("You don't have enough USDT in the wallet !")
-                short_add += 0
-            else:
-                short_add += short_ask
+            #Long
+            if df.loc[i,"risk_long"] < -0.50 and df.loc[i,"risk_long"] > -0.8:
+                df.loc[i,"state_long"] = "Called"
+                print(df)
+                print(f"Hey, {user_long.name}, you just got a margin call ! Add more funds !")
                 
-        elif df.loc[i,"risk_short"] < -0.8:
-            df.loc[i,"state_short"] = "Liquidated"
-            print("Your position has been liquidated !")
-            
-        elif df.loc[i,"risk_long"] > -0.5:
-             short_add += 0
- 
-    #New orders so the order book keeps running
-    new_buy_orders=create_orders(market_price=i)
-    for order in range(3):
-        order_book_b.loc[len(order_book_b)+i] = new_buy_orders[order]
-    order_book_b = order_book_b.sort_values(by=['Price'], ascending=False).reset_index(drop=True)
-    
-    new_sell_orders=create_orders(False,market_price=i)
-    for order in range(3):
-        order_book_s.loc[len(order_book_s)+i] = new_sell_orders[order]
-    order_book_s = order_book_s.sort_values(by=['Price']).reset_index(drop=True)
+                #Long agrega fondos
+                long_ask = float(input("Add USDT to margin:"))
+                
+                if long_ask > user_long.usdt - init_margin_long:
+                    print("You don't have enough USDT in the wallet !")
+                    long_add += 0
+                else:
+                    long_add += long_ask
+        
+                
+            elif df.loc[i,"risk_long"] < -0.8:                 
+                df.loc[i,"state_long"] = "Liquidated"
+                print("Your position has been liquidated !")
+                
+                
+            elif df.loc[i,"risk_long"] > -0.5:
+                 long_add += 0
+                
+            #Short
+            if df.loc[i,"risk_short"] < -0.50 and df.loc[i,"risk_short"] > -0.8:
+                df.loc[i,"state_short"] = "Called"
+                print(f"Hey, {user_short.name}, you just got a margin call ! Add more funds !")
+                
+                #Short agrega fondos
+                short_ask = float(input("Add USDT to margin:"))    
+                
+                if short_ask > user_short.usdt - init_margin_short:
+                    print("You don't have enough USDT in the wallet !")
+                    short_add += 0
+                else:
+                    short_add += short_ask
+                    
+            elif df.loc[i,"risk_short"] < -0.8:
+                df.loc[i,"state_short"] = "Liquidated"
+                print("Your position has been liquidated !")
+                
+            elif df.loc[i,"risk_long"] > -0.5:
+                 short_add += 0
+     
+        #New orders so the order book keeps running
+        new_buy_orders=create_orders(market_price=i)
+        for order in range(3):
+            order_book_b.loc[len(order_book_b)+i] = new_buy_orders[order]
+        order_book_b = order_book_b.sort_values(by=['Price'], ascending=False).reset_index(drop=True)
+        
+        new_sell_orders=create_orders(False,market_price=i)
+        for order in range(3):
+            order_book_s.loc[len(order_book_s)+i] = new_sell_orders[order]
+        order_book_s = order_book_s.sort_values(by=['Price']).reset_index(drop=True)
+                
+        yield
 
-    #User Inputs; ODERS
-    
+
+  
+def loop(tareas):
+    while tareas:
+        print(tareas)
+        actual = tareas.pop(0)
+        try:
+            print('-')
+            next(actual)
+            tareas.append(actual)
+        except StopIteration:
+            print(f'{actual} stopped iteration')
+            pass
+        
+loop([main(), add_user_order(146, 'long', 10, 10)])
+
     
 
 
