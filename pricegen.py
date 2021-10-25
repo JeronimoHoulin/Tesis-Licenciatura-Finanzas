c36de0585006718d30bfdc42b5b16b5bd81e4131
Jrossi11 <71575042+Jrossi11@users.noreply.github.com>
2021-10-25 20:03:36 +0200

    Update pricegen.py

--- pricegen.py
+++ pricegen.py
@@ -6,6 +6,9 @@ import time
 ticker = 'AAPL'
 sub = yf.download(f"{ticker}",period="1m")['Close'][0]
 
+df1 = pd.DataFrame(columns = ["Entry Price", "Exit Price", "Direction", "P&L", "Balance"])
+df1.to_csv('order_history.csv')
+
 def brownian_motion(df, u, rf, num_days, sigma):  
     dt = 1/360
     price = df.loc[df.index[-1]][0]*(1+u*dt + sigma * np.random.normal(0, 1) * np.sqrt(dt))
@@ -14,7 +17,7 @@ def brownian_motion(df, u, rf, num_days, sigma):
 price_series = pd.DataFrame(sub,columns=['price'], index=[0])
 while True:
     brownian_motion(price_series, 0.1, 0.025, 1, 0.1)
-    time.sleep(1)
+    time.sleep(2)
     if len(price_series) > 50:
         price_series = price_series[-50:]
     price_series.to_csv('sub.csv')
