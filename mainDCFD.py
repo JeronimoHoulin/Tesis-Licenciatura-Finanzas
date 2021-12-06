0ba69b985df5a84aa2385fb3694f86d5c44597d9
Jrossi11 <71575042+Jrossi11@users.noreply.github.com>
2021-12-06 18:57:21 +0100

    Update mainDCFD.py

--- mainDCFD.py
+++ mainDCFD.py
@@ -1,14 +1,13 @@
-"""
-Created on Sat Sep 18 09:49:34 2021
-Jero & Juan
-"""
+""" Created on Sat Sep 18 09:49:34 2021 Jero & Juan """
+
 import pandas as pd
+import numpy as np
+import time
 import streamlit as st
 import random as rd
 import matplotlib.pyplot as plt
 import string
 import plotly.express as px
-import time
 
 plt.style.use('dark_background')
 
@@ -57,14 +56,13 @@ trades_slot = col1.empty()
 class user:    
     def __init__(self, name, wallet_id, usdt): 
         # Donde balance es el balance de stablecoins en la billetera de un usuario
-        # Y la dirección es 1 para "LONG" y -1 para "SHORT"
         self.name = name
         self.wallet_id = wallet_id
         self.usdt = usdt
         self.active = False
   
 # Creamos un smart contract vacio para ser usado para el settlement
-class smart_contract: ##WEB3 PARA ESTO.
+class smart_contract:
     def __init__(self, address, balance):
         self.address = address
         self.balance = balance
@@ -92,42 +90,50 @@ subyacente = pd.read_csv('sub.csv')
 
 
 ''' Order Book '''
-def create_orders(market_price,buy=True):
-    if buy:
-        buy_orders = [market_price*(1-i/1500) for i in range(3)]
-        buy_orders_size = [rd.randint(1, i) for i in range(100,10,-30)]
-        address = [''.join(rd.choice(adress_gen) for i in range(10)) for j in range(3)]
-        return [list(i) for i in zip(buy_orders, buy_orders_size, address)]
-    else:
-        sell_orders = [market_price*(1+i/1500) for i in range(3)]
-        sell_orders_size = [rd.randint(1, i) for i in range(100,10,-30)]
-        address = [''.join(rd.choice(adress_gen) for i in range(10)) for j in range(3)]
-        return [list(i) for i in zip(sell_orders,sell_orders_size, address)]
+def create_orders(market_price,b, side):
+    if side == 'buy':
+        n = rd.randint(1,b)
+        buy_orders_price = [market_price*(1-i/200) for i in range(n)]
+        buy_orders_size = [rd.randint(1, i**2) for i in range(1,n+1)]
+        address = [''.join(rd.choice(adress_gen) for i in range(10)) for j in range(n)]
+        return pd.DataFrame({'Price': buy_orders_price, 'Size':buy_orders_size, 'Address': address})
+
+    if side == 'sell':
+        n = rd.randint(1,b)
+        sell_orders_price = [market_price*(1+i/200) for i in range(n)]
+        sell_orders_size = [rd.randint(1, i**2) for i in range(1,n+1)]
+        address = [''.join(rd.choice(adress_gen) for i in range(10)) for j in range(n)]
+        return pd.DataFrame({'Price': sell_orders_price, 'Size':sell_orders_size, 'Address': address})  
 
 
-order_book_b = pd.DataFrame(create_orders(subyacente['price'][0]), columns=['Price', 'Size', 'Address']).sort_values(by=['Price'], ascending=False).reset_index(drop=True)
-order_book_s = pd.DataFrame(create_orders(subyacente['price'][0],False), columns=['Price', 'Size', 'Address']).sort_values(by=['Price']).reset_index(drop=True)
+order_book_b = pd.DataFrame(create_orders(subyacente['price'][0], 2, 'buy'), columns=['Price', 'Size', 'Address']).sort_values(by=['Price'], ascending=False).reset_index(drop=True)
+order_book_s = pd.DataFrame(create_orders(subyacente['price'][0], 2, 'sell'), columns=['Price', 'Size', 'Address']).sort_values(by=['Price']).reset_index(drop=True)
 
 trades = pd.DataFrame(columns=['Price','Size', 'MP', 'Contract id'])
-
+trades.index.name = 'trade'
+trade_n = 0
 def match_orders(i):
-    global order_book_b, order_book_s
-    while max(order_book_b['Price']) >= min(order_book_s['Price']):
-        if order_book_b['Price'][0] >= order_book_s['Price'][0]:
-            price = order_book_b['Price'][0]
-            size = min(order_book_b['Size'][0], order_book_s['Size'][0])
-            contract_id = order_book_b['Address'][0]
-            trades.loc[contract_id]=[price, size, i, contract_id]
-            info_slot.subheader(f'\n {size} DCFDs entered at ${round(price,2)}')
-            
-            order_book_b.loc[0,'Size'] -= size 
-            order_book_s.loc[0,'Size'] -= size 
-            
-            if order_book_b.loc[0,'Size'] == 0:
-                order_book_b = order_book_b.drop(0).reset_index(drop=True)
+    global order_book_b, order_book_s, trades, trade_n
+    try:
+        while max(order_book_b['Price']) >= min(order_book_s['Price']):
+            trade_n += 1 
+            if order_book_b['Price'][0] >= order_book_s['Price'][0]:
+                price = (order_book_s['Price'][0]+order_book_b['Price'][0])/2
+                size = min(order_book_b['Size'][0], order_book_s['Size'][0])
+                contract_id = order_book_b['Address'][0]
+                trades.loc[trade_n]=[price, size, i, contract_id]
+                info_slot.subheader(f'\n {size} DCFDs entered at ${round(price,2)}')
                 
-            if order_book_s.loc[0,'Size'] == 0:
-                order_book_s = order_book_s.drop(0).reset_index(drop=True)
+                order_book_b.loc[0,'Size'] -= size 
+                order_book_s.loc[0,'Size'] -= size 
+                
+                if order_book_b.loc[0,'Size'] == 0:
+                    order_book_b = order_book_b.drop(0).reset_index(drop=True)
+                    
+                if order_book_s.loc[0,'Size'] == 0:
+                    order_book_s = order_book_s.drop(0).reset_index(drop=True)
+    except:
+        print('No trades to match')
 
 def init_trade_df(i, init_margin): # Creamos el DF que muestra la evolucion del trade. Magins, PnL and Risk levels
     df = pd.DataFrame()
@@ -145,16 +151,14 @@ def main():
     global order_book_b, order_book_s, market_price, df, subyacente, leverage, cantidad, buy, sell, closep, df1
     while True:
         # LEYENDO Y GRAFICANDO EL MOVIMIENTO DEL SUBYACENTE GENERADO POR PRICEGEN NOTEBOOK
-        
         try:
             subyacente = pd.read_csv('sub.csv')
         except:
             time.sleep(0.01)
-            subyacente = pd.read_csv('sub.csv') 
+            subyacente = pd.read_csv('sub.csv')
             
         fig = px.line(subyacente, x= subyacente.index, y=['price','Ema-5', 'Ema-20'], color_discrete_sequence = ['orange','red', 'green'], title='Underlying Asset Price')
         plot_slot.plotly_chart(fig)
-        
         i = round(subyacente.loc[subyacente.index[-1],'price'],3)
         
         ## BUY orders ##
@@ -188,13 +192,14 @@ def main():
         ## CLOSING orders ##   
         if closep == True:
             df = pd.read_csv('df.csv')
+            
             if df1.loc[df1.index[-1], 'Direction'] == 'Long': 
                 smart_contract.send_back_long() ##hacer que se envie en margen a wallet del smart.
                 df1 = df1.append({'Entry Price':df.Price[df.index[0]],'Exit Price':df.Price[df.index[-1]], 'Quantity':cantidad, 'Direction': 'Long',
                                   'P&L': df.margin_long[df.index[-1]]-df.margin_long[df.index[0]], 'Balance': df.Balance[df.index[-1]]}, ignore_index=True)
                 df1.to_csv('order_history.csv')
                 order_history.dataframe(df1)
-                
+                print('Se ejecuto esto')
                 user.active = False
                 buy = False
                 closep = False
@@ -205,7 +210,7 @@ def main():
                                   'P&L': df.margin_short[df.index[-1]]-df.margin_short[df.index[0]], 'Balance': df.Balance[df.index[-1]]}, ignore_index=True)
                 df1.to_csv('order_history.csv')
                 order_history.dataframe(df1)
-                
+                print('Se ejecuto esto otro')
                 user.active = False
                 sell = False
                 closep = False
@@ -221,10 +226,12 @@ def main():
 
             order_history.dataframe(df1)
 
-            df.loc[i,'Price'] = trades['Price'][len(trades)-1]
+            #df.loc[i,'Price'] = trades['Price'][len(trades)-1]
+            df.loc[i,'Price'] = trades.loc[trade_n ,'Price']
             
             df.loc[i,"risk_long"] = df.loc[i,"margin_long"] / df.loc[df.index[0],"margin_long"]
             df.loc[i,"risk_short"] = df.loc[i,"margin_short"] / df.loc[df.index[0],"margin_short"]
+            print(df.risk_long, df.risk_short)
             
             df.loc[i, 'pnl_long'] = (df.iloc[-1,0] - df.iloc[-2,0]) * cantidad
             df.loc[i,'pnl_short'] = -(df.iloc[-1,0] - df.iloc[-2,0]) * cantidad
@@ -299,21 +306,17 @@ def main():
                  
     
         #New orders so the order book keeps running
-        new_buy_orders=create_orders(i, True)
-        for order in range(3):
-            order_book_b.loc[len(order_book_b)+i] = new_buy_orders[order]
-        order_book_b = order_book_b.sort_values(by=['Price'], ascending=False).reset_index(drop=True)
-        
-        new_sell_orders=create_orders(i,False)
-        for order in range(3):
-            order_book_s.loc[len(order_book_s)+i] = new_sell_orders[order]
-        order_book_s = order_book_s.sort_values(by=['Price']).reset_index(drop=True)
+        new_buy_orders = create_orders(i, 2, 'buy')
+        order_book_b = order_book_b.append(new_buy_orders).sort_values(by=['Price'], ascending=False).reset_index(drop=True).head(9)
+
+        new_sell_orders = create_orders(i, 2, 'sell')
+        order_book_s = order_book_s.append(new_sell_orders).sort_values(by=['Price']).reset_index(drop=True).tail(9)
         
         # Displaying things in Streamlit 
         metric_slot.metric(label='Underlying Price', value=round(i,2), delta=round(i-subyacente.loc[subyacente.index[-2], 'price'],3))
-        trades_slot.dataframe(trades)
-        sell_df.dataframe(order_book_s.sort_values(by=['Price'], ascending=False).reset_index(drop=True)[:9])
-        buy_df.dataframe(order_book_b[:9])
+        trades_slot.dataframe(trades.sort_values(by='trade', ascending=False))
+        sell_df.dataframe(order_book_s.sort_values(by=['Price'], ascending=False).reset_index(drop=True))
+        buy_df.dataframe(order_book_b)
         
         if len(df1)>0: #Si existe el df de order history tomar el balance de ahi.
             user.usdt = df1.Balance[df1.index[-1]]
@@ -334,5 +337,10 @@ def event_loop(tareas):
             pass
         
 event_loop([main()])
+
+
+
+
+
 
 
